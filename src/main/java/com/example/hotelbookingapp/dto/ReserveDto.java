36e71b07679a7c7d28228cfef933de7ad1f3bace
Micaela <m.andrea.higa@gmail.com>
2022-04-19 18:49:00 -0300

    Add ReserveDto.java

---
+++ src/main/java/com/example/hotelbookingapp/dto/ReserveDto.java
@@ -0,0 +1,31 @@
+package com.example.hotelbookingapp.dto;
+
+import com.example.hotelbookingapp.model.Room;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class ReserveDto {
+
+    @NotBlank
+    private List<Integer> listRooms;
+    @NotBlank
+    private LocalDate bookingCheckin;
+    @NotBlank
+    private LocalDate bookingCheckout;
+    @NotBlank
+    private Integer bookingBreakfast;
+    @NotBlank
+    private Integer fkGuestId;
+    @NotBlank
+    private String bookingNotes;
+    @NotBlank
+    private String status;
+    @NotBlank
+    private Integer paymentMethod;
+}
